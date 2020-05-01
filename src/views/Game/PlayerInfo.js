2a7f0d690c5a3775c57b015743521daca3c30908
B Severino <biancasev@gmail.com>
2020-05-01 12:00:02 -0400

    Removed console logs and commented out code

--- src/views/Game/PlayerInfo.js
+++ src/views/Game/PlayerInfo.js
@@ -1,52 +1,42 @@
 import React from 'react';
-import {
-  Box,
-  Flex,
-  Stack,
-  Text,
-  Image,
-  Code,
-} from '@chakra-ui/core';
-import { useSelector } from 'react-redux'
+import { Box, Flex, Stack, Text, Image, Code } from '@chakra-ui/core';
+import { useSelector } from 'react-redux';
 
 export default function PlayerInfo() {
-    const { user } = useSelector(state => state.game)
+  const { user } = useSelector((state) => state.game);
 
-    return (
-        <Flex h={175}>
-            <Box width={150}>
-                <Image
-                    src={user.portrait}
-                    fallbackSrc="https://via.placeholder.com/150"
-                />
-            </Box>
-            <Stack pl={3}>
-                <Text>
-                    <strong>{user.character_name}</strong>,{' '}
-                    <span style={{ textTransform: "capitalize" }}>
-                        {user.character_type}
-                    </span>
-                </Text>
-                <Text>
-                    <strong>HP:</strong> {user.HP}{' '}
-                    <strong>MP:</strong> {user.MP}
-                </Text>
-                <Text>
-                    <strong>Gold:</strong> {user.gold}
-                </Text>
-                <Text>
-                    <strong>Inventory:</strong>{' '}
-                    {
-                        user.items ?
-                            user.items.split(' ').map((item, index) => (
-                                <span key={index}>
-                                    <Code>{item}</Code>{' '}
-                                </span>
-                            ))
-                        : 'Empty!'
-                    }
-                </Text>
-            </Stack>
-        </Flex>
-    );
-};
+  return (
+    <Flex h={175}>
+      <Box width={150}>
+        <Image
+          src={user.portrait}
+          fallbackSrc="https://via.placeholder.com/150"
+        />
+      </Box>
+      <Stack pl={3}>
+        <Text>
+          <strong>{user.character_name}</strong>,{' '}
+          <span style={{ textTransform: 'capitalize' }}>
+            {user.character_type}
+          </span>
+        </Text>
+        <Text>
+          <strong>HP:</strong> {user.HP} <strong>MP:</strong> {user.MP}
+        </Text>
+        <Text>
+          <strong>Gold:</strong> {user.gold}
+        </Text>
+        <Text>
+          <strong>Inventory:</strong>{' '}
+          {user.items
+            ? user.items.split(' ').map((item, index) => (
+                <span key={index}>
+                  <Code>{item}</Code>{' '}
+                </span>
+              ))
+            : 'Empty!'}
+        </Text>
+      </Stack>
+    </Flex>
+  );
+}
